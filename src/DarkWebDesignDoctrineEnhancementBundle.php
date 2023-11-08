8d94b8d9abacc227b025121df7f3784de5d64a92
Raymond Schouten <raymondschouten@users.noreply.github.com>
2023-11-08 08:28:36 +0000

    #20: Added Symfony 5.x support

--- src/DarkWebDesignDoctrineEnhancementBundle.php
+++ src/DarkWebDesignDoctrineEnhancementBundle.php
@@ -31,4 +31,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class DarkWebDesignDoctrineEnhancementBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 }
--- src/DependencyInjection/DarkWebDesignDoctrineEnhancementExtension.php
+++ src/DependencyInjection/DarkWebDesignDoctrineEnhancementExtension.php
@@ -40,7 +40,7 @@ class DarkWebDesignDoctrineEnhancementExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
     }
 }
